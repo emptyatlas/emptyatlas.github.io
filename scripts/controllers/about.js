4b04b02849a773772f1108eccdea2b3ed97ed535
Jeana Smith <jelsmith@cspire.com>
2016-04-19 16:17:51 -0500

    initial commit

---
+++ scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name emptyatlasgithubioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the emptyatlasgithubioApp
+ */
+angular.module('emptyatlasgithubioApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name emptyatlasgithubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the emptyatlasgithubioApp
+ */
+angular.module('emptyatlasgithubioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
