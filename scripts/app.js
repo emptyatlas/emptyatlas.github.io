bfba2ffb790f8bf1ac73396bf7f08a13223ba6c4
Jeana Smith <jelsmith@cspire.com>
2016-04-26 09:51:41 -0500

    Optimize

--- scripts/app.js
+++ scripts/app.js
@@ -8,33 +8,44 @@
  *
  * Main module of the application.
  */
-angular
-  .module('emptyatlasgithubioApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/store', {
-        templateUrl: 'views/store.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .when('/news', {
-        templateUrl: 'views/news.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      }).when('contact-form.php', {})
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+angular.module('emptyatlasgithubioApp', ['ngAnimate', 'ngCookies', 'ngResource', 'ngRoute', 'ngSanitize', 'ngTouch']).config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl',
+      controllerAs: 'main'
+    })
+    .when('/store', {
+      templateUrl: 'views/store.html',
+      controller: 'MainCtrl',
+      controllerAs: 'about'
+    })
+    .when('/news', {
+      templateUrl: 'views/news.html',
+      controller: 'MainCtrl',
+      controllerAs: 'about'
+    }).when('contact-form.php', {})
+    .otherwise({
+      redirectTo: '/'
+    });
+});
+
+/**
+ * @ngdoc function
+ * @name emptyatlasgithubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the emptyatlasgithubioApp
+ */
+angular.module('emptyatlasgithubioApp').controller('MainCtrl', function($scope) {
+
+  $scope.showContactForm = function() {
+    _gscq.push(['trackPage','/contact']);
+  };
+
+  $scope.hideMenu = function() {
+    angular.element('#js-navbar-collapse').collapse('hide');
+    return true;
+  };
+
+});
