d97356eecdc0cdc04be40fafb56a6d2a08d5c807
Jeana Smith <jelsmith@cspire.com>
2017-03-29 21:27:32 -0500

    NEW THEME NEW THEME YES

--- scripts/app.js
+++ scripts/app.js
@@ -8,26 +8,18 @@
  *
  * Main module of the application.
  */
-angular.module('emptyatlasgithubioApp', ['ngAnimate', 'ngCookies', 'ngResource', 'ngRoute', 'ngSanitize', 'ngTouch', 'angular-click-outside']).config(function ($routeProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl: 'views/main.html',
-            controller: 'MainCtrl',
-            controllerAs: 'main'
-        })
-        .when('/store', {
-            templateUrl: 'views/store.html',
-            controller: 'MainCtrl',
-            controllerAs: 'about'
-        })
-        .when('/news', {
-            templateUrl: 'views/news.html',
-            controller: 'MainCtrl',
-            controllerAs: 'about'
-        }).when('contact-form.php', {})
-        .otherwise({
-            redirectTo: '/'
-        });
+angular.module('emptyatlasgithubioApp', ['ngAnimate', 'ngCookies', 'ngResource', 'ngRoute', 'ngSanitize', 'ngTouch', 'angular-click-outside']).config(function ($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix('');
+
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('contact-form.php', {})
+            .otherwise({
+                redirectTo: '/'
+            });
 }).run(function () {});
 
 /**
@@ -37,38 +29,42 @@ angular.module('emptyatlasgithubioApp', ['ngAnimate', 'ngCookies', 'ngResource',
  * # MainCtrl
  * Controller of the emptyatlasgithubioApp
  */
-angular.module('emptyatlasgithubioApp').controller('MainCtrl', function ($scope, $templateCache, $window, MerchService, NewsService) {
+angular.module('emptyatlasgithubioApp').controller('MainCtrl', function ($scope, $templateCache, $window) {
 
-    $scope.$on('$viewContentLoaded', function () {
-        $window.scrollTo(0, 0);
-    });
+    $(document).ready(function () {
+            $('.parallax').parallax();
+        });
 
-    $scope.showContactForm = function () {
-        _gscq.push(['trackPage', '/contact']);
-    };
+        $scope.$on('$viewContentLoaded', function () {
+            $window.scrollTo(0, 0);
+        });
 
-    $scope.hideMenu = function () {
-        angular.element('#js-navbar-collapse').collapse('hide');
-        return true;
-    };
+        $scope.showContactForm = function () {
+            _gscq.push(['trackPage', '/contact']);
+        };
 
-    $scope.openLink = function (link) {
-        console.log('attempting to open link [' + link + ']');
-        $window.open(link, '_blank');
-    };
+        $scope.hideMenu = function () {
+            angular.element('#js-navbar-collapse').collapse('hide');
+            return true;
+        };
 
-    $scope.merchList = MerchService.merchList;
-    $scope.squareStoreLink = MerchService.squareStoreLink;
-    $scope.newsList = NewsService.newsList;
-    $scope.latestNews = NewsService.newsList.slice(0, 2);
+        $scope.openLink = function (link) {
+            console.log('attempting to open link [' + link + ']');
+            $window.open(link, '_blank');
+        };
 
-    $scope.getCurrentYear = function () {
-        try {
-            return new Date().getFullYear();
-        } catch (err) {
-            console.error(err);
-            return "2017";
-        }
-    };
+//        $scope.merchList = MerchService.merchList;
+//        $scope.squareStoreLink = MerchService.squareStoreLink;
+//        $scope.newsList = NewsService.newsList;
+//        $scope.latestNews = NewsService.newsList.slice(0, 2);
+
+        $scope.getCurrentYear = function () {
+            try {
+                return new Date().getFullYear();
+            } catch (err) {
+                console.error(err);
+                return "2017";
+            }
+        };
 
 });
