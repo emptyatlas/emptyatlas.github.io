9b5049cca6a2e0e81e26623febd4ded62e2ab8c2
Jeana Smith <jelsmith@cspire.com>
2020-07-17 10:30:46 -0500

    - Create a dedicated News page - Add additional news articles and quotes - Remove any references to Google Play - Minor cosmetic tweaks - Update README.md with Kairos links

--- README.md
+++ README.md
@@ -1,18 +1,21 @@
 # emptyatlasgithubio
 
-Website for the indie rock band, Empty Atlas.
+Website for the melodic, indie-rock band, Empty Atlas.
 [Visit now](https://emptyatlas.com)
 
-## New single “SHORT FICTION” available everywhere you can hear things
+## New album "KAIROS" available everywhere you can hear things
 Listen now on any major platform:
-* [Spotify](https://open.spotify.com/track/10dP2wOiZgRXC7cku6nrpy)
-* [Apple Music](http://itunes.apple.com/album/id/1412982650)
-* [iTunes](http://itunes.apple.com/album/id1412982650?ls=1&app=itunes)
-* [Google Play](https://play.google.com/store/music/album/Empty_Atlas_Short_Fiction?id=Bb3soszugorqekta7yew4vfbe34)
-* [Amazon Music](https://www.amazon.com/Short-Fiction-Empty-Atlas/dp/B07FMLJTJF/ref=sr_1_2)
-* [Sound Cloud](https://soundcloud.com/empty-atlas/short-fiction-empty-atlas-single)
+* [Spotify](https://open.spotify.com/album/1R6XM9xoyFz1iRUaRATuid)
+* [Apple Music](https://music.apple.com/us/album/kairos/1512448187)
+* [iTunes](https://music.apple.com/us/album/kairos/1512448187?ls=1&app=itunes)
+* [Tidal](https://tidal.com/browse/album/140616400)
+* [YouTube Music](https://music.youtube.com/playlist?list=OLAK5uy_n2GxDo7LgrkZK5BNZyq_HAGGDrOeQRMPg)
+* [Amazon Music](https://www.amazon.com/Kairos-Empty-Atlas/dp/B088BH853N/ref=sr_1_1)
+* [Sound Cloud](https://soundcloud.com/empty-atlas)
+* [Bandcamp](https://emptyatlas.bandcamp.com/)
 
 ## Follow
 * [Facebook](http://facebook.com/emptyatlasmusic)
 * [Instagram](http://instagram.com/emptyatlas)
 * [YouTube](https://www.youtube.com/user/emptyatlasmusic)
+* [Shop](https://squareup.com/store/empty-atlas-music)
--- _site/scripts/app.js
+++ _site/scripts/app.js
@@ -39,6 +39,10 @@ var app = angular.module('emptyatlasgithubioApp', ['ngRoute', 'ngSanitize']).con
       templateUrl: 'views/listen.html',
       controller: 'MainCtrl'
     })
+    .when('/news', {
+      templateUrl: 'views/news.html',
+      controller: 'MainCtrl'
+    })
     .otherwise({
       redirectTo: '/'
     });
@@ -61,7 +65,7 @@ app.service("ModuleService", function () {
  * # MainCtrl
  * Controller of the emptyatlasgithubioApp
  */
-app.controller('MainCtrl', function ($scope, $rootScope, $log, $templateCache, $window, $document, $timeout, $sce, $location, LyricService, ModuleService, VideoService) {
+app.controller('MainCtrl', function ($scope, $rootScope, $log, $templateCache, $window, $document, $timeout, $sce, $location, $filter, LyricService, ModuleService, VideoService) {
 
   // This link is separated to fool spam bots
   $scope.sendMail = 'mai' + 'lto' + ':' + 'hello' + '@emptyatlas.com';
@@ -77,7 +81,6 @@ app.controller('MainCtrl', function ($scope, $rootScope, $log, $templateCache, $
   $scope.iTunesUrl = 'https://music.apple.com/us/album/kairos/1512448187?ls=1&app=itunes';
   $scope.appleMusicUrl = 'https://music.apple.com/us/album/kairos/1512448187';
   $scope.appleMusicEmbedUrl = 'https://embed.music.apple.com/us/album/kairos/1512448187';
-  $scope.googlePlayUrl = 'https://play.google.com/store/music/artist/Empty_Atlas?id=Af5xacnwu5cxcedk4myhfbt2pay';
   $scope.spotifyUrl = 'https://open.spotify.com/album/1R6XM9xoyFz1iRUaRATuid';
   $scope.spotifyEmbedUrl = 'https://open.spotify.com/embed/album/1R6XM9xoyFz1iRUaRATuid';
   $scope.amazonMusicUrl = 'https://www.amazon.com/Kairos-Empty-Atlas/dp/B088BH853N/ref=sr_1_1';
@@ -107,7 +110,6 @@ app.controller('MainCtrl', function ($scope, $rootScope, $log, $templateCache, $
       iTunesUrl: $sce.trustAsResourceUrl($scope.iTunesUrl),
       soundCloudUrl: $sce.trustAsResourceUrl($scope.soundCloudUrl),
       youTubeMusicUrl: $sce.trustAsResourceUrl($scope.youTubeMusicUrl),
-      googlePlayUrl: $sce.trustAsResourceUrl($scope.googlePlayUrl),
       amazonMusicUrl: $sce.trustAsResourceUrl($scope.amazonMusicUrl),
       tidalUrl: $sce.trustAsResourceUrl($scope.tidalUrl)
     },
@@ -122,7 +124,6 @@ app.controller('MainCtrl', function ($scope, $rootScope, $log, $templateCache, $
       iTunesUrl: $sce.trustAsResourceUrl('https://music.apple.com/us/album/maximal-single/1506115438?ls=1&app=itunes'),
       soundCloudUrl: undefined,
       youTubeMusicUrl: $sce.trustAsResourceUrl('https://music.youtube.com/watch?v=6poLggcs9ww&feature=share'),
-      googlePlayUrl: $sce.trustAsResourceUrl('https://play.google.com/store/music/album/Empty_Atlas_Maximal?id=Bs4jnnxm7e63fkg7xa5tq2iakqm&hl=en_US'),
       amazonMusicUrl: $sce.trustAsResourceUrl('https://www.amazon.com/Maximal-Empty-Atlas/dp/B086QJTF4X/ref=sr_1_1'),
       tidalUrl: $sce.trustAsResourceUrl('https://tidal.com/browse/track/136269864')
     },
@@ -137,7 +138,6 @@ app.controller('MainCtrl', function ($scope, $rootScope, $log, $templateCache, $
       iTunesUrl: $sce.trustAsResourceUrl('https://music.apple.com/us/album/short-fiction-single/1412982650?ls=1&app=itunes'),
       soundCloudUrl: $sce.trustAsResourceUrl('https://soundcloud.com/empty-atlas/short-fiction-empty-atlas-single'),
       youTubeMusicUrl: $sce.trustAsResourceUrl('https://music.youtube.com/watch?v=VXz2cdKXXH8&feature=share'),
-      googlePlayUrl: $sce.trustAsResourceUrl('https://play.google.com/store/music/album/Empty_Atlas_Short_Fiction?id=Bb3soszugorqekta7yew4vfbe34&hl=en_US'),
       amazonMusicUrl: $sce.trustAsResourceUrl('https://www.amazon.com/dp/B07FMLJTJF/ref=cm_sw_em_r_mt_dp_U_25fVEbBTQMP9Y'),
       tidalUrl: $sce.trustAsResourceUrl('https://tidal.com/browse/track/92107037?play=true')
     },
@@ -152,7 +152,6 @@ app.controller('MainCtrl', function ($scope, $rootScope, $log, $templateCache, $
       iTunesUrl: $sce.trustAsResourceUrl('https://itunes.apple.com/us/album/hestia/1172021169'),
       soundCloudUrl: $sce.trustAsResourceUrl('https://soundcloud.com/empty-atlas/sets/hestia-full-album-stream'),
       youTubeMusicUrl: $sce.trustAsResourceUrl('https://music.youtube.com/playlist?list=OLAK5uy_kfaL5Pr94FGRO-jA236UAsfQz5nAa8ThI'),
-      googlePlayUrl: $sce.trustAsResourceUrl('https://play.google.com/store/music/album/Empty_Atlas_Hestia?id=Bhygwq7rdqe6sabtmvvescbk4ce'),
       amazonMusicUrl: $sce.trustAsResourceUrl('https://www.amazon.com/dp/B01N1NZFMU/ref=cm_sw_em_r_mt_dp_U_vfGyCbENVXSZJ'),
       tidalUrl: undefined
     },
@@ -167,7 +166,6 @@ app.controller('MainCtrl', function ($scope, $rootScope, $log, $templateCache, $
       iTunesUrl: undefined, //$sce.trustAsResourceUrl('https://itunes.apple.com/us/album/anniversary/902603298'),
       soundCloudUrl: undefined,
       youTubeMusicUrl: $sce.trustAsResourceUrl('https://music.youtube.com/playlist?list=OLAK5uy_kaIwdpuMJgBQodx_wtpM1FrfBZwyjujSU'),
-      googlePlayUrl: $sce.trustAsResourceUrl('https://play.google.com/store/music/album/Empty_Atlas_Anniversary?id=B27zpkjzpokqbd3rngky5vtptia'),
       amazonMusicUrl: undefined,
       tidalUrl: $sce.trustAsResourceUrl('https://tidal.com/browse/album/50568535?play=true')
     },
@@ -182,19 +180,49 @@ app.controller('MainCtrl', function ($scope, $rootScope, $log, $templateCache, $
       iTunesUrl: $sce.trustAsResourceUrl('https://itunes.apple.com/us/album/holiday-parties-single/1445540114'),
       soundCloudUrl: undefined,
       youTubeMusicUrl: $sce.trustAsResourceUrl('https://music.youtube.com/playlist?list=OLAK5uy_nvc6x42AAEgFiPgerNMIDx0M-Bme3EB6c'),
-      googlePlayUrl: $sce.trustAsResourceUrl('https://play.google.com/store/music/album/Empty_Atlas_Holiday_Parties?id=Bukgabgp4m4kxtfhwzdvgp6v4oe'),
       amazonMusicUrl: $sce.trustAsResourceUrl('https://www.amazon.com/dp/B07L45TC6V/ref=cm_sw_em_r_mt_dp_U_VlGyCb7GQXVTQ'),
       tidalUrl: $sce.trustAsResourceUrl('https://tidal.com/browse/track/100389767?play=true')
     }
   ];
 
+  var newsArticleTemplate = {
+    title: '',
+    desc: ``,
+    img: '',
+    actionText: 'READ MORE',
+    date: 123,
+    link: ''
+  };
+
+  var newsQuoteTemplate = {
+    quote: ``,
+    author: '',
+    date: 123,
+    link: ''
+  };
+
   var maxCharLength = 148;
   $scope.news = [
+    {
+      title: 'Turtle Tempo',
+      desc: `Empty Atlas are personifying the ancient philosophy that ‘the only constant is change’ with their new album Kairos. Taking a refreshed approach to their songwriting, the band note the differences between their debut and sophomore records; where Hestia centred on settling down, Kairos is all about shaking things up and seeking change. “Maximal” is the lead singe of their most recent release, taking a heartfelt approach to the subject of chasing one’s dreams.`,
+      img: 'images/news/turtle-tempo.png',
+      actionText: 'READ MORE',
+      date: 1594904400000,
+      link: 'https://turtletempo.co.uk/new-music-empty-atlas-maximal/'
+    },
     {
       title: 'Right Chord Music',
       desc: `Empty Atlas’ new full-length album, Kairos, is the perfect soundtrack for a long drive somewhere new, or for a sleepy afternoon spent watching the ocean.`,
       img: 'images/news/right-chord-music.png',
       actionText: 'READ MORE',
+      date: 1594299600000,
+      link: 'https://www.rightchordmusic.co.uk/empty-atlas-maximal/?fbclid=IwAR36VvA-EYFj7D_XL206XgQIMed3GI3DdrDo0JpvNVRVJD2PCzSRe8vihm0'
+    },
+    {
+      quote: `The perfect soundtrack for a long drive somewhere new, or for a sleepy afternoon spent watching the ocean.`,
+      author: 'Rachel Makinson, Right Chord Music',
+      date: 1594299600000,
       link: 'https://www.rightchordmusic.co.uk/empty-atlas-maximal/?fbclid=IwAR36VvA-EYFj7D_XL206XgQIMed3GI3DdrDo0JpvNVRVJD2PCzSRe8vihm0'
     },
     {
@@ -202,6 +230,13 @@ app.controller('MainCtrl', function ($scope, $rootScope, $log, $templateCache, $
       desc: `One of my favorite discoveries last year was Jackson, Mississippi's own Empty Atlas. Their single "Short Fiction" made me an instant fan, and their new album Kairos does not disappoint.`,
       img: 'images/news/jesus-freak-hideout.png',
       actionText: 'READ MORE',
+      date: 1591880400000,
+      link: 'https://www.jesusfreakhideout.com/indiemusic/Kairos.asp?fbclid=IwAR2shu4uhkyH4MYWyjUPvcRxnJOBxxI4wJtC2WPiY7gXyWNvzlgVzCts6Q8'
+    },
+    {
+      quote: `One of my favorite discoveries... Their single 'Short Fiction' made me an instant fan, and their new album Kairos does not disappoint.`,
+      author: 'Scott Fryberger, Jesus Freak Hideout',
+      date: 1591880400000,
       link: 'https://www.jesusfreakhideout.com/indiemusic/Kairos.asp?fbclid=IwAR2shu4uhkyH4MYWyjUPvcRxnJOBxxI4wJtC2WPiY7gXyWNvzlgVzCts6Q8'
     },
     {
@@ -209,13 +244,37 @@ app.controller('MainCtrl', function ($scope, $rootScope, $log, $templateCache, $
       desc: `Ally Rincón interviews Micah Smith of Jackson based alt-rock band Empty Atlas. Watch as they talk about the band's new single "Maximal," their upcoming album, and how the pandemic has affected the music industry!`,
       img: 'images/news/all-punked-up.jpg',
       actionText: 'WATCH NOW',
+      date: 1588078800000,
       link: 'https://www.youtube.com/watch?v=KMQpaIUTdOE&feature=youtu.be&fbclid=IwAR2-MtTTF89r6gjdUVdYiVoAto6CmDx3_lKJouBXkbcFgmdTPY_v4-lOsOk'
+    },
+    {
+      title: 'Best of Jackson Finalist',
+      desc: `Formed in Brandon, Mississippi, in 2014, Empty Atlas has been slowly winning over a wide-reaching fan base throughout the South with its energetic, melodic indie-rock sound and its heavier lyrical core.`,
+      img: 'images/news/best-of-jackson-2020.png',
+      actionText: 'READ MORE',
+      date: 1577887200000,
+      link: 'http://www.bestofjackson.com/empty-atlas/'
+    },
+    {
+      title: 'SINGLE PREMIERE: Stream "Maximal" by Empty Atlas',
+      desc: `Jackson, Mississippi alternative rock band Empty Atlas is premiering their new single “Maximal” right here on Stitched Sound! The quartet has spent the last several months working on new music, and this single is the first track to be released from their upcoming sophomore studio album Kairos.`,
+      img: 'images/news/stitched-sound-2020.png',
+      actionText: 'READ MORE',
+      date: 1586955600000,
+      link: 'https://stitchedsound.com/maximal-empty-atlas-premiere/?fbclid=IwAR0Bze62SkezvhkiiFkPYDe1RvsQ0Yd3wS3gQuJk6iRcXfBleWzfzBOjNI0'
     },
     {
       title: 'FBT Entertainment',
       desc: `Mississippi alternative rock band, Empty Atlas, dropped their new single yesterday titled "Maximal" to tease their upcoming sophomore record Kairos.`,
       img: 'images/news/fbt-entertainment.png',
       actionText: 'READ MORE',
+      date: 1587042000000,
+      link: 'https://fbtentertainment.com/blogs/news/new-release-maximal-by-empty-atlas'
+    },
+    {
+      quote: `[Maximal] makes you want to press repeat & experience the rollercoaster of feelings all over again.`,
+      author: 'Deirdre Kelly, FBT Entertainment',
+      date: 1587042000000,
       link: 'https://fbtentertainment.com/blogs/news/new-release-maximal-by-empty-atlas'
     },
     {
@@ -223,13 +282,58 @@ app.controller('MainCtrl', function ($scope, $rootScope, $log, $templateCache, $
       desc: `It seems like just yesterday that we had Empty Atlas‘ Micah Smith and Brennan Michael White on one of our early Quarantine Jams. That night, they told us about their then-forthcoming single, “Maximal,” set for release on April 24.`,
       img: 'images/news/underground-music-collective.png',
       actionText: 'READ MORE',
+      date: 1588078800000,
       link: 'https://undergroundmusiccollective.com/2020/04/27/the-quinns-daily-spinn-tuesday-april-28/'
+    },
+    {
+      title: 'PREMIERE: Empty Atlas share their deeply infectious new single Short Fiction',
+      desc: `Since dropping their debut double EP back in 2014, Mississippi outfit Empty Atlas have carved out a unique space for themselves within the realm of indie rock.`,
+      img: 'images/news/happy-mag-2018.png',
+      actionText: 'READ MORE',
+      date: 1532696400000,
+      link: 'https://happymag.tv/premiere-empty-atlas-short-fiction/'
+    },
+    {
+      title: 'MPB\'s Amped & Wired',
+      desc: `Jackson, Mississippi’s Empty Atlas performs melodic indie rock with a lyrical focus on personal growth and connections.`,
+      img: 'images/news/amped-and-wired-2018.png',
+      actionText: 'WATCH NOW',
+      date: 1524229200000,
+      link: 'https://video.mpbonline.org/video/empty-atlas-qfmwic/'
+    },
+    {
+      title: 'Mississippi\'s Empty Atlas opening for Young Valley',
+      desc: `Mississippi-based alternative rockers Empty Atlas are making the trip across the river for their Natural State debut on Friday. The four-piece will open for fellow Jackson outfit Young Valley at Maxine's in Hot Springs.`,
+      img: 'images/news/arkansas-democratic-gazette.png',
+      actionText: 'READ MORE',
+      date: 1517493600000,
+      link: 'https://www.arkansasonline.com/news/2018/feb/01/mississippi-s-empty-atlas-opening-for-y/'
+    },
+    {
+      title: 'Music Makers Presents: Micah Smith',
+      desc: `Interview and live acoustic set with lead singer, Micah Smith.`,
+      img: 'images/news/music-makers-presents-2017.png',
+      actionText: 'WATCH NOW',
+      date: 1491483600000,
+      link: 'https://vimeo.com/212136518'
+    },
+    {
+      title: 'Interview with Empty Atlas',
+      desc: `Mississippi guitar rock without blues roots, cover art with the conceptual resonance of a classic concept album, and likeable melodic rock without the love songs… these surprises all serve to distinguish Empty Atlas from any preconception you might hazard from if supplied with only the mere facts of their origin.`,
+      img: 'images/news/magic-city-bands-2017.png',
+      actionText: 'READ MORE',
+      date: 1498827600000,
+      link: 'https://magiccitybands.com/coming-empty-atlas/'
     }
   ];
 
+  $scope.isQuoteDefined = function(newsItem) {
+    return newsItem.quote === undefined;
+  }
+
   // Trim the description for the news articles to make sure they're evenly distributed.
   angular.forEach($scope.news, function (n) {
-    if (n.desc.length !== maxCharLength) {
+    if (n.desc !== undefined && n.desc.length >= maxCharLength) {
       var trimmedDesc = n.desc.slice(0, maxCharLength) + '...';
       n.desc = trimmedDesc;
     }
